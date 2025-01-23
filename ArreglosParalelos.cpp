32ab1b713028ca2109e3213a18f35f4cdda749c4
Lili Morales <61772690+Castrocina@users.noreply.github.com>
2025-01-22 23:49:51 -0600

    Add files via upload

--- ArreglosParalelos.cpp
+++ ArreglosParalelos.cpp
@@ -1,12 +1,15 @@
 // ArreglosParalelos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
+
+// iostream - Librería que permite trabajar con operaciones de entrada y salida de la consola
 #include <iostream>
+// omp - Librería que permite ejecutar programación paralela empleando mútiples hilos
 #include <omp.h>
 
-#define N 1000
-#define chunk 100
-#define mostrar 10
+// Declaración de variables
+#define N 1000 // Tamaño de los arreglos
+#define chunk 100 // variable empleada para dividir las iteraciones en bloques 
+#define mostrar 10 // Listado máximo de elementos a desplegar en pantalla
 
 void imprimeArreglo(float *d);
 
@@ -23,13 +26,19 @@ int main()
     
     int pedazos = chunk;
 
+    // #pragma omp parallel for - Permite paralelizar los bucles for dividiendo las iteraciones entre varios hilos
+    //shared - Permite especificar las variables que seran compartidas entre los hilos
+    // private(i) - Permite indicar que la variable será privada para cada hilo, lo permite llevar a cabo la iteración
+    // schedule static - Controla como se distribuyen las iteraciones del bucle entre los hilos
     #pragma omp parallel for \
     shared(a, b, c, pedazos) private(i) \
     schedule(static, pedazos)
 
+    // Se realiza la suma de los arreglos
     for (i = 0; i < N; i++)
         c[i] = a[i] + b[i];
 
+    // Se imprimen resultados en la Consola
     std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
     imprimeArreglo(a);
     std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
@@ -39,6 +48,7 @@ int main()
 
 }
 
+// Los datos a imprimir de los arreglos son separados por guiones y acotados a máximo 10 valores en pantalla
 void imprimeArreglo(float *d) {
     for (int x = 0; x < mostrar; x++)
         std::cout << d[x] << " - ";
