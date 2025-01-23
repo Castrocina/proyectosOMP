eee6667a867858956ca89f5f7f4b71a21b683cc8
Lili Morales <61772690+Castrocina@users.noreply.github.com>
2025-01-23 00:41:11 -0600

    Add files via upload

--- ArreglosParalelos.cpp
+++ ArreglosParalelos.cpp
@@ -5,10 +5,11 @@
 #include <iostream>
 // omp - Librería que permite ejecutar programación paralela empleando mútiples hilos
 #include <omp.h>
+#include <random> // Librería para generar números aleatorios
 
 // Declaración de variables
 #define N 1000 // Tamaño de los arreglos
-#define chunk 100 // variable empleada para dividir las iteraciones en bloques 
+#define chunk 100 // Variable empleada para dividir las iteraciones en bloques 
 #define mostrar 10 // Listado máximo de elementos a desplegar en pantalla
 
 void imprimeArreglo(float *d);
@@ -19,9 +20,16 @@ int main()
     float a[N], b[N], c[N];
     int i;
 
+    // Inicialización del generador de números aleatorios para el índice
+    std::random_device rd;  // Semilla no determinista
+    std::mt19937 gen(rd()); // Generador de Mersenne Twister
+    std::uniform_int_distribution<> distrib(0, 1000); // Distribución aleatoria para índices
+
+
     for (i = 0; i < N; i++) {
-        a[i] = i * 10;
-        b[i] = (i + 3) * 3.7;
+        int random_index = distrib(gen);  // Genera un índice aleatorio
+        a[i] = random_index * 10;
+        b[i] = (random_index + 3) * 3.7;
     }
     
     int pedazos = chunk;
@@ -30,9 +38,7 @@ int main()
     //shared - Permite especificar las variables que seran compartidas entre los hilos
     // private(i) - Permite indicar que la variable será privada para cada hilo, lo permite llevar a cabo la iteración
     // schedule static - Controla como se distribuyen las iteraciones del bucle entre los hilos
-    #pragma omp parallel for \
-    shared(a, b, c, pedazos) private(i) \
-    schedule(static, pedazos)
+    #pragma omp parallel for shared(a, b, c, pedazos) private(i) schedule(static, pedazos)
 
     // Se realiza la suma de los arreglos
     for (i = 0; i < N; i++)
